bf97b9025fc7f4f55b5973673d362f976be8df20
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-08-20 20:27:15 +0530

    refatcoring

--- BootJPAExample/src/main/java/com/jpa/test/BootJpaExampleApplication.java
+++ BootJPAExample/src/main/java/com/jpa/test/BootJpaExampleApplication.java
@@ -1,25 +1,19 @@
 package com.jpa.test;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jpa.test.dao.UserRepository;
-import com.jpa.test.entities.User;
-
 @SpringBootApplication
-public class BootJp aExampleApplication {
+public class BootJpaExampleApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context= (ApplicationContext) SpringApplication.run(BootJpaExampleApplication.class, args);
-		UserRepository userRepository=((BeanFactory) context).getBean(UserRepository.class);
-		User user=new User();
-		user.setName("Sagar Kumar");
-		user.setCity("Giridih");
+		 SpringApplication.run(BootJpaExampleApplication.class, args);
+		 
+		// UserRepository userRepo=context.getBean(UserRepository.class);
+				 
+		 User user=new User(1,"sk","giridih");
 		
-		User user1=userRepository.save(user);
-		System.out.println(user1);
+		 
 		
 		
 	}
