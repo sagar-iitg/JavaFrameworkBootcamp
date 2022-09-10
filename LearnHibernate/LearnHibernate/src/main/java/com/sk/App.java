70902a7aa76b2f3c966d27f4f5cba583a1487986
sagar <sagar-iitg>
2022-09-11 00:44:04 +0530

    Fetch Demo

--- LearnHibernate/LearnHibernate/src/main/java/com/sk/App.java
+++ LearnHibernate/LearnHibernate/src/main/java/com/sk/App.java
@@ -1,5 +1,10 @@
 package com.sk;
 
+
+
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,7 +16,7 @@ import org.hibernate.cfg.Configuration;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Project Statrted" );
         
@@ -22,19 +27,43 @@ public class App
         
         
         //create Student
-        Student st=new Student(1,"sagar","Giridih");
+        Student st=new Student(1,"sagar kumar","Giridih");
         Student st1=new Student();
-        st1.setId(3);
-        st1.setCity("Muzzafarpur");
-        st1.setName("Ankit");
+        Student st2=new Student();
+        st2.setId(3);
+        st2.setCity("Muzzafarpur");
+        st2.setName("Ankit kumar");
+        st1.setId(2);
+        st1.setCity("Belda");
+        st1.setName("Mohit agarwala");
+        
+        
+        //creating object of address class
+        Address address=new Address();
+        address.setStreet("Manihari Patti");
+        address.setCity("Giridih");
+        address.setOpen(true);
+        address.setAddedDate(new Date());
+        address.setX(1234.440);
+        
+        //ReadingImage
+//        FileInputStream fis=new FileInputStream("src/main/java/pic.jpg");
+//        byte[] data=new byte[fis.available()];
+//        fis.read(data);
+//        address.setImage(data);
+        
         
         Session session=f.openSession();
         
         Transaction tx=session.beginTransaction();
         session.save(st1);
+        session.save(st2);
+        session.save(st);
+        session.save(address);
         tx.commit();
         
         session.close();
+        System.out.println("Project Done");
         
         
 //        System.out.println(f.toString());
